7168c74fd254223b7a5f68c8dee82da4550318bd
iBabity <luckas.ribeiro2015@gmail.com>
2020-05-07 14:19:51 -0300

    upload de arquivos com capybara

---
+++ qafullstack/ruby/capy/spec/upload_spec.rb
@@ -0,0 +1,36 @@
+describe 'upload', :upload do
+
+#  Para este sript que necessita de acesso ao diretorio de arquivos 
+#  e imagens, e aconselhavel que fique alocado no disco C para maior facilidade
+#  de encontrar os arquivos na hora do upload. Isto somente em SO windows.
+
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/upload'
+        @arquivo = Dir.pwd + '/spec/fixtures/arquivo.txt'
+        @imagem = Dir.pwd +  '/spec/fixtures/imagem.png'
+    end
+
+    it "upload de texto" do
+
+        attach_file('file-upload', @arquivo)
+        click_button 'Upload'
+
+        div_arquivo = find('#uploaded-file')
+        expect(div_arquivo.text).to eql 'arquivo.txt'
+    end
+
+
+    it "upload de imagem" do
+
+        attach_file('file-upload', @imagem)
+        click_button 'Upload'
+
+        sleep 5
+        img = find('#new-image')
+        expect(img[:src]).to include '/uploads/imagem.png'
+    end
+
+    after(:each) do
+        sleep 5
+    end 
+end 
