c14d9ff47c8add6ca61378c8252d85ceec2c2db4
iBabity <luckas.ribeiro2015@gmail.com>
2020-05-13 15:41:19 -0300

    mouse hover com capybara

---
+++ qafullstack/ruby/capy/spec/hover_spec.rb
@@ -0,0 +1,36 @@
+describe 'mouse hover', :hovers do
+
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/hovers'
+    end
+
+    it 'Quando passar o mouse sobre o blade' do
+
+        card = find('img[alt*=Blade]')
+        card.hover
+
+        expect(page).to have_content 'Nome: Blade'
+    end 
+
+    it 'Quando passar o mouse sobre o pantera negra' do
+
+        card = find('img[alt^=Pantera]')
+        card.hover
+
+        expect(page).to have_content 'Nome: Pantera Negra'
+    end 
+
+
+    it 'Quando passar o mouse sobre o Homem aranha' do
+
+        card = find('img[alt$=Aranha]')
+        card.hover
+
+        expect(page).to have_content 'Nome: Homem Aranha'
+    end 
+
+    after(:each) do
+        sleep 0.5
+    end 
+
+end
