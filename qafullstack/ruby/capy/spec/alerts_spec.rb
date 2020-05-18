870da7a2efcb0aab6a7633663a1731f7d361eaf9
iBabity <luckas.ribeiro2015@gmail.com>
2020-05-18 16:33:54 -0300

    alertas em js

---
+++ qafullstack/ruby/capy/spec/alerts_spec.rb
@@ -0,0 +1,61 @@
+describe 'alertas de js', :alertas do
+
+    before(:each) do
+        visit "https://training-wheels-protocol.herokuapp.com/javascript_alerts"
+    end
+
+
+    it 'alerta', :alerta do
+
+        click_button 'Alerta'
+        msg = page.driver.browser.switch_to.alert.text
+        puts msg
+        expect(msg).to eql 'Isto é uma mensagem de alerta'
+        sleep 2
+    end 
+
+    it 'sim confirma', :confirma do
+
+        click_button 'Confirma'
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'E ai confirma?'
+        sleep 2
+
+        page.driver.browser.switch_to.alert.accept
+        expect(page).to have_content 'Mensagem confirmada'
+        sleep 3
+    end 
+
+    it 'nao confirma', :confirma do
+
+        click_button 'Confirma'
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'E ai confirma?'
+        sleep 2
+
+        page.driver.browser.switch_to.alert.dismiss
+        expect(page).to have_content 'Mensagem não confirmada'
+        sleep 3
+    end 
+
+    it 'aceitando o prompt', :prompt do
+
+        accept_prompt(with: 'Lucas') do
+            click_button 'Prompt'
+            sleep 2
+        end
+
+        expect(page).to have_content 'Olá, Lucas'
+        sleep 3
+    end 
+    
+    it 'negando o prompt', :prompt do 
+
+        dismiss_prompt(with: 'Ana') do
+            click_button 'Prompt'
+            sleep 2
+        end 
+        expect(page).to have_content 'Olá, null'
+    end 
+
+end
---
+++ qafullstack/ruby/capy/spec/key_spec.rb
@@ -0,0 +1,29 @@
+describe 'teclado', :key do
+
+    before(:each) do
+
+        visit "https://training-wheels-protocol.herokuapp.com/key_presses"
+    end
+
+    it 'enviando simbolos' do
+
+        simbolos = %i[tab escape space enter shift control alt home end]
+
+        simbolos.each do |t|
+            find('#campo-id').send_keys t
+            expect(page).to have_content 'You entered: ' + t.to_s.upcase
+            sleep 1
+        end
+    end
+
+    it "enviando letras" do
+
+        letras = %w[a s d f g h j k l]
+
+        letras.each do |l|
+            find('#campo-id').send_keys l
+            expect(page).to have_content 'You entered: ' + l.to_s.upcase
+        sleep 1
+        end 
+    end 
+end 
