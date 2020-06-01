9c5c5839f6c7bccdc91c78ce17454a0390e044ff
iBabity <luckas.ribeiro2015@gmail.com>
2020-06-01 15:40:59 -0300

    specs ruby

---
+++ qafullstack/ruby/capy/spec/iframes_spec.rb
@@ -0,0 +1,36 @@
+describe 'iframes' do 
+
+    describe 'iframe bom', :nice do
+
+        before(:each) do 
+            visit = 'https://training-wheels-protocol.herokuapp.com/nice_iframe'
+        end 
+
+        it'adicionar ao carrinho' do
+            within_frame('burgerId') do
+                produto = find('.menu-item-info-box-text', text: 'Refrigerante')
+                produto.find('a').click
+                expect(find('#cart')).to have_content 'R$ 4,50'
+                sleep 5
+            end
+        end
+    end
+
+    describe 'iframe ruim', :bad do 
+        
+        before(:each) do 
+            visit = 'https://training-wheels-protocol.herokuapp.com/bad_iframe'
+        end 
+
+        it 'carrinho esta vazio' do 
+        
+            script = '$(".box-iframe").attr("id", "tempId");'
+            page.execute_script(script)
+
+            within_frame('tempId') do
+                expect(find('#cart')).to have_content 'Seu carrinho está vazio!'
+                sleep 3
+            end
+        end
+    end
+end
